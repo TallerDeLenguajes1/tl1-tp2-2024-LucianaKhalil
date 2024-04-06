832e26b607a209ab04492332dfe85bea103239b9
Luciana <khalilmarialuciana@gmail.com>
2024-04-05 22:49:54 -0300

    segundo commit

---
+++ tp2_1_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// codigo a completar
+#define N 20
+
+int main(){
+    srand(time(NULL));
+    int i;
+    double vt[N];
+    double *punt;
+    punt=vt;
+
+    for(i = 0;i<N; i++)
+{
+
+    vt[i]=1+rand()%100;
+    printf("%f ", *punt);
+    punt++;
+}
+return 0;
+}
+
